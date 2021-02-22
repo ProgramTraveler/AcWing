c4b62af8b27f69c3c62dcfeeb149cafa122b3741
PrpgramTraveler <2115655391@qq.com>
2021-02-22 23:15:52 +0800

    2021-02-22

---
+++ 17.从头到尾打印链表.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> printListReversingly(ListNode* head) {
+        vector<int> ans;
+        while (head != nullptr) {
+            ans.push_back(head -> val);
+            head = head -> next;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
