5a06da6a7dd867d225d60b40a07883e7a6899407
PrpgramTraveler <2115655391@qq.com>
2022-10-03 19:50:24 +0800

    2022-10-03

---
+++ 双链表/827.双链表.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int m;
+int e[N], l[N], r[N], idx;
+
+// 初始化
+void init () {
+    // 0表示左端点 1表示右端点
+    r[0] = 1, l[0] = 0;
+    idx = 2;
+}
+
+// 在下标是 k 的点的右边插入 x (如果是在左边插入 其实也可以直接调用这个 但参数需要换一下) 
+void add (int k, int x) { //顺序别写反了
+    e[idx] = x;
+    r[idx] = r[k];
+    l[idx] = k;
+    l[r[k]] = idx;
+    r[k] = idx;
+}
+
+// 删除操作
+void remove (int k, int x) {
+    r[l[k]] = r[k];
+    l[r[k]] = l[k];
+
+}
