82470018d161b28c335daa5c99e102d1e7b933bf
PrpgramTraveler <2115655391@qq.com>
2022-10-04 16:04:01 +0800

    2022-10-04

---
+++ 栈/828.模拟栈.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int stk[N], tt; // tt 表示栈顶元素
+
+// 插入
+stk[++ tt] = x;
+
+// 弹出
+tt --;
+
+// 判断栈是否为空
+if (tt > 0) not empty
+esle empty
+
+// 栈顶
+stk[tt];
---
+++ 队列/829.模拟队列.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int q[N], hh, tt = -1; // hh 表示的是队头 tt 表示的是队尾 (注意这里栈初始的是 -1 而栈初始的是0)
+
+// 插入一个元素
+q[ ++ tt] = x;
+
+// 弹出一个元素
+hh ++;
+
+// 判断是否为空
+if (hh <= tt) not empty
+else empty
+
+// 取出队头元素
+q[hh]
