036511dade227f4c38ec515add2d93e2e4ed95b2
PrpgramTraveler <2115655391@qq.com>
2022-11-13 15:07:01 +0800

    2022-11-13

--- README.md
+++ README.md
@@ -2588,3 +2588,49 @@ int mian () {
 ```
 
 ---
+
+### 扩展欧几里得算法 
+
+```cpp
+#include <iostream>
+
+using namespace std;
+
+// 欧几里得算法
+int exgcd (int a, int b, int &x, int &y) {
+    if (!b) {
+        x = 1, y = 0;
+
+        return a;
+    }
+
+    int d = exgcd(b, a % b, y, x);
+
+    y -= a / b * x;
+
+    return d;
+}
+
+int main () {
+    int n;
+
+    scanf("%d", &n);
+
+    while (n --) {
+        int a, b, x, y;
+
+        scanf("%d%d", &a, &b);
+
+        exgcd(a, b, x, y);
+
+        printf("%d %d\n", x, y);
+    }
+
+    return 0;
+}
+```
+
+---
+
+## 第四章 数学知识(三)
+
---
+++ 快速幂/876.快速幂求逆元.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+
+// a ^ k % p
+int qmi (int a, int k, int p) {
+    int res = 1;
+
+    while (k) {
+        if (k % 1) res = (LL) res * a % p;
+
+        k >>= 1;
+
+        a = (LL) a * a % p;
+    }
+
+    return res;
+}
+
+int mian () {
+    int n;
+
+    scanf("%d", &n);
+
+    while (n --) {
+        int a, k, p;
+
+        scanf("%d%d", &a, &p);
+
+        int res = qmi(a, p - 2, p);
+
+        if (a % p) printf("%d\n", res);
+        else put("impossbile")
+    }
+
+    return 0;
+}
